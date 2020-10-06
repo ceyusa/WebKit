25d358e3d54de78c55fb4a450840af251a7a2116
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2020-10-06 15:37:16 +0200

    add ccls in dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,11 +7,19 @@
   (c-basic-offset . 4))
  (c++-mode
   (indent-tabs-mode . nil)
-  (c-basic-offset . 4))
+  (c-basic-offset . 4)
+  (mode . clang-format+))
  (java-mode
   (indent-tabs-mode . nil)
   (c-basic-offset . 4))
  (ruby-mode
   (ruby-indent-level . 4))
  (change-log-mode
-  (indent-tabs-mode . nil)))
+  (indent-tabs-mode . nil))
+ (nil
+  (fill-column . 100)
+  (ccls-executable . "/home/vjaquez/.local/bin/webkit-ccls")
+  (ccls-initialization-options . (:compilationDatabaseDirectory "/app/webkit/WebKitBuild/Release"
+                                  :cache (:directory "/app/webkit/.ccls-cache")))
+  (compile-command . "build-webkit --gtk --debug")
+  (lsp-enable-file-watchers . nil)))
